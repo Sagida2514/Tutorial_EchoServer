321398585450387a791306590a90b98282210554
Sagida2514 <90020964+Sagida2514@users.noreply.github.com>
2023-12-17 16:29:06 +0900

    에코서버

---
+++ EchoServer/MainServer.cs
@@ -0,0 +1,108 @@
+﻿using EchoServerEx;
+using NLog;
+using SuperSocket.SocketBase;
+using SuperSocket.SocketBase.Config;
+using SuperSocket.SocketBase.Logging;
+using SuperSocket.SocketBase.Protocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoServer
+{
+    class MainServer : AppServer<NetworkSession, EFBinaryRequestInfo>
+    {
+        public static ILog MainLogger;
+
+        Dictionary<int, Action<NetworkSession, EFBinaryRequestInfo>> HandlerMap = new Dictionary<int, Action<NetworkSession, EFBinaryRequestInfo>>();
+        CommonHandler CommonHan = new CommonHandler();
+
+        IServerConfig m_Config;
+
+        public MainServer()
+                : base(new DefaultReceiveFilterFactory<ReceiveFilter, EFBinaryRequestInfo>())
+        {
+            NewSessionConnected += new SessionHandler<NetworkSession>(OnConnected);
+            SessionClosed += new SessionHandler<NetworkSession, CloseReason>(OnClosed);
+            NewRequestReceived += new RequestHandler<NetworkSession, EFBinaryRequestInfo>(RequestReceived);
+        }
+
+        public void InitConfig(ServerOption option)
+        {
+            m_Config = new ServerConfig
+            {
+                Port = option.Port,
+                Ip = "Any",
+                MaxConnectionNumber = option.MaxConnectionNumber,
+                Mode = SocketMode.Tcp,
+                Name = option.Name
+            };
+        }
+
+        public void CreateServer()
+        {
+            try
+            {
+                bool bResult = Setup(new RootConfig(), m_Config, logFactory: new NLogLogFactory());
+
+                if (bResult == false)
+                {
+                    Console.WriteLine("[ERROR] 서버 네트워크 설정 실패 ㅠㅠ");
+                    return;
+                }
+                else
+                {
+                    MainLogger = base.Logger;
+                }
+
+                //RegistHanlder();
+
+                MainLogger.Info("서버 생성 성공");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[ERROR] 서버 생성 실패: {ex.ToString()}");
+            }
+        }
+
+        public bool IsRunning(ServerState eCurState)
+        {
+            if(eCurState == ServerState.Running)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        void OnConnected(NetworkSession session)
+        {
+            MainLogger.Info($"세션 번호 {session.SessionID} 접속");
+        }
+
+        void OnClosed(NetworkSession session, CloseReason reason)
+        {
+            MainLogger.Info($"세션 번호 {session.SessionID} 접속해제: {reason.ToString()}");
+        }
+
+        void RequestReceived(NetworkSession session, EFBinaryRequestInfo reqInfo)
+        {
+            var PacketID = reqInfo.PacketID;
+
+            if (HandlerMap.ContainsKey(PacketID))
+            {
+                HandlerMap[PacketID](session, reqInfo);
+            }
+            else
+            {
+                MainLogger.Info($"세션 번호 {session.SessionID}, 받은 데이터 크기: {reqInfo.Body.Length}");
+            }
+        }
+    }
+
+    public class NetworkSession : AppSession<NetworkSession, EFBinaryRequestInfo>
+    {
+    }
+}
--- EchoServer/Program.cs
+++ EchoServer/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 
 namespace EchoServer
 {
@@ -6,7 +7,55 @@ namespace EchoServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello SuperSocketLite");
+
+            var serverOption = ParseCommandLine(args);
+            if(serverOption == null)
+            {
+                return;
+            }
+
+            var server = new MainServer();
+            server.InitConfig(serverOption);
+            server.CreateServer();
+
+            var IsResult = server.Start();
+
+            if(IsResult)
+            {
+                MainServer.MainLogger.Info("서버 네트워크 시작");
+            }
+            else
+            {
+                Console.WriteLine("[ERROR] 서버 네트워크 시작 실패");
+                return;
+            }
+
+            MainServer.MainLogger.Info("key를 누르면 종료한다....");
+            Console.ReadKey();
         }
+
+        static ServerOption ParseCommandLine(string[] args)
+        {
+            var result = CommandLine.Parser.Default.ParseArguments<ServerOption>(args) as CommandLine.Parsed<ServerOption>;
+            
+            if(result == null)
+            {
+                Console.WriteLine("Failed Command Line");
+                return null;
+            }
+
+            return result.Value;
+        }
+    }
+
+    public class ServerOption
+    {
+        [Option("port", Required = true, HelpText = "Server Port Number")]
+        public int Port { get; set; }
+        [Option("maxConnectionNumber", Required = true, HelpText = "MaxConnectionNumber Count")]
+        public int MaxConnectionNumber { get; set; }
+        [Option("name", Required = true, HelpText = "Server Name")]
+        public string Name { get; set; }
     }
 }
